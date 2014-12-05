e31a938883a3dc64f88a1d185e4edfb9daca6510
William <xf0718@gmail.com>
2014-12-05 15:17:34 +0800

    using interface

--- lc/cp.c
+++ lc/cp.c
@@ -3,6 +3,8 @@
 #include <unistd.h>
 #include <fcntl.h>
 
+#include "except/error.h"
+
 #define PERMS 0644
 #define BUFSIZE 32
 
@@ -13,23 +15,17 @@ int main(int argc, char *argv[])
     int n;
 
     if (argc != 3) {
-        fprintf(stderr, "Usage: cp from to\n");
-        exit(1);
+        error("Usage: cp from to");
     }
     if ((f1 = open(argv[1], O_RDONLY, 0)) == -1) {
-        fprintf(stderr, "cp: can't open %s\n", argv[1]);
-        exit(1);
+        error("cp: can't open %s\n", argv[1]);
     }
     if ((f2 = creat(argv[2], PERMS)) == -1) {
-        fprintf(stderr, "cp: can't create %s, mode %03o\n",
-                argv[2], PERMS);
-        exit(1);
+        error("cp: can't create %s, mode %03o\n", argv[2], PERMS);
     }
     while ((n = read(f1, buf, BUFSIZE)) > 0) {
         if (write(f2, buf, n) != n) {
-            fprintf(stderr, "cp: write error on file %s\n",
-                    argv[2]);
-            exit(1);
+            error("cp: write error on file %s\n", argv[2]);
         }
     }
     return 0;
